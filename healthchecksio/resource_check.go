7473da200dc1585bad047f330769fea1659d8d2e
Takashi Masuda <masutaka.net@gmail.com>
2018-09-03 22:08:04 +0900

    - Support parameter `timeout`

--- healthchecksio/resource_check.go
+++ healthchecksio/resource_check.go
@@ -28,6 +28,11 @@ func resourceHealthcheck() *schema.Resource {
 				Optional:    true,
 				Elem:        &schema.Schema{Type: schema.TypeString},
 			},
+			"timeout": &schema.Schema{
+				Type:        schema.TypeInt,
+				Description: "Timeout expected period of the healthcheck",
+				Optional:    true,
+			},
 			"grace": &schema.Schema{
 				Type:        schema.TypeInt,
 				Description: "Grace period for the healthcheck",
@@ -96,6 +101,7 @@ func resourceHealthcheckRead(d *schema.ResourceData, m interface{}) error {
 
 	d.Set("name", healthcheck.Name)
 	d.Set("tags", strings.Split(healthcheck.Tags, " "))
+	d.Set("timeout", healthcheck.Timeout)
 	d.Set("grace", healthcheck.Grace)
 	d.Set("schedule", healthcheck.Schedule)
 	d.Set("timezone", healthcheck.Timezone)
@@ -116,7 +122,7 @@ func resourceHealthcheckUpdate(d *schema.ResourceData, m interface{}) error {
 
 	log.Printf("[DEBUG] healthcheck update: %#v", healthcheck)
 
-	if d.HasChange("tags") || d.HasChange("grace") || d.HasChange("schedule") || d.HasChange("timezone") {
+	if d.HasChange("tags") || d.HasChange("timeout") || d.HasChange("grace") || d.HasChange("schedule") || d.HasChange("timezone") {
 		_, err = client.Update(key, *healthcheck)
 		if err != nil {
 			return fmt.Errorf("Failed to update healthcheck: %s", err)
@@ -149,6 +155,10 @@ func createHealthcheckFromResourceData(d *schema.ResourceData) (*healthchecksio.
 	if attr, ok := d.GetOk("tags"); ok {
 		tags := toSliceOfString(attr.([]interface{}))
 		healthcheck.Tags = strings.Join(tags, " ")
+	}
+
+	if attr, ok := d.GetOk("timeout"); ok {
+		healthcheck.Timeout = attr.(int)
 	}
 
 	if attr, ok := d.GetOk("grace"); ok {
