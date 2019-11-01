c99d296079fb51c89862f6229b73ec5015807768
Kristoffer Ahl <git@77dynamite.com>
2019-11-01 14:42:54 +0100

    - Updated readme and minimal example.

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -6,3 +6,11 @@ resource "healthchecksio_check" "bare_minimum" {
   name    = "test-bare-minimum-check"
   timeout = 86400
 }
+
+output "ping_url" {
+  value = healthchecksio_check.bare_minimum.ping_url
+}
+
+output "pause_url" {
+  value = healthchecksio_check.bare_minimum.pause_url
+}
